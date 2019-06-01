c46b6d54d5c125ba7ecf46bff016744b9566d126
1bt <37535974+1bt@users.noreply.github.com>
2019-06-01 20:41:10 +0800

    第三章

--- CH3.c
+++ CH3.c
@@ -42,3 +42,65 @@ void main()
 	printf("%-8.2lf\n", x);
 }*/
 
+/* 例3-4 */
+/*格式输入函数的应用*/
+/*#include <stdio.h>
+void main()
+{
+	int a, b, m, n;
+	long c;
+	float x, y;
+	double z;
+	char ch1, ch2;
+	scanf_s("%d,%d,%ld", &a, &b, &c);
+	printf("a=%d,b=%d,c=%ld\n", a, b, c);
+	fflush(stdin);
+	scanf_s("%2d%*2d%3d", &m, &n);
+	printf("m=%d,n=%d\n", m, n);
+	fflush(stdin);
+	scanf("%f,%e,%lf"， & x, &y, &z);
+	printf("x=%f,y=%f,z=%lf\n", x, y, z);
+	fflush(stdin);
+	scanf("%c%c", ch1, ch2);
+	printf("%c%c", ch1, ch2);
+}*/
+
+/* 例3-5 */
+/*已知圆柱体的底面半径和高，求圆柱体的体积*/
+/*#include <stdio.h>
+#define PI 3.1415926
+void main()
+{
+	float r, h, v;
+	printf("input r h:\n");
+	scanf_s("%f%f", &r, &h);
+	v = PI * r * r * h;
+	printf("v=%.3f\n", v);
+}*/
+
+/* 例3-6 */
+/*交换器*/
+/*#include <stdio.h>
+void main()
+{
+	int a, b, t;
+	printf("请输入两个整数：\n");
+	scanf_s("%d%d", &a, &b);
+	printf("交换前a=%d,b=%d\n", a, b);
+	t = a; a = b; b = t;
+	printf("交换后a=%d,b=%d\n", a, b);
+}*/
+
+/* 例3-7 */
+/*已知三角形的三边长，求三角形的面积*/
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+	float a, b, c;
+	float t, s;
+	scanf_s("%f%f%f", &a, &b, &c);
+	t = (a + b + c) / 2;
+	s = sqrt(t * (t - a) * (t - b) * (t - c));
+	printf("s=%.3f", s);
+}
