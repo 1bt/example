f6105ded1612fd1a13414f998089b448f1e3ecc2
1bt <37535974+1bt@users.noreply.github.com>
2019-06-01 17:08:56 +0800

    第三章

---
+++ CH3.c
@@ -0,0 +1,20 @@
+/* Àı3-1 */
+/*cÓïÑÔ¾ÙÀı*/
+/*#include <stdio.h>
+#include <math.h>
+void main()
+{
+	float x, y;
+	scanf_s("%f", &x);
+	if (x < 0)
+	{
+		x = -x;
+		y = sqrt(x + 6);		
+	}
+	else
+	{
+		y = 1;
+	}
+	printf("y=%f", y);
+}*/
+
