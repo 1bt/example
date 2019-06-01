23264f215fbce4f90d1d892c805303989d0a68a1
1bt <37535974+1bt@users.noreply.github.com>
2019-06-01 18:53:33 +0800

    第三章

--- CH3.c
+++ CH3.c
@@ -18,3 +18,27 @@ void main()
 	printf("y=%f", y);
 }*/
 
+/* 例3-2 */
+/*计算x的面积*/
+/*#include <stdio.h>
+#include <math.h>
+void main()
+{
+	float x, y;
+	x = 1, 5;
+	y = sin(x);
+	printf("%f\n", y);
+}*/
+
+/* 例3-3 */
+/*格式修饰符顺序举例*/
+/*#include <stdio.h>
+void main()
+{
+	double x = 123.456;
+	long int a = -1, b = 123;
+	printf("%#15lo\n", a);
+	printf("%08ld\n", b);
+	printf("%-8.2lf\n", x);
+}*/
+
